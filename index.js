212cef79e61ceb452309c9aea746c4629edef5d4
chino702 <chinox3702@gmail.com>
2023-04-30 17:05:53 -0700

    adding behavior with methods

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Your code here
+class Cat {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    speak() {
+      return `${this.name} says meow!`;
+    }
+  }
+  
+  class Dog {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    speak() {
+      return `${this.name} says woof!`;
+    }
+  }
+  
+  class Bird {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    speak() {
+      if (this.name === 'Mable') {
+        return 'Mable says squawk!';
+      } else {
+        return `It's me! ${this.name}, the parrot!`;
+      }
+    }
+  }
+  
+  
+  
